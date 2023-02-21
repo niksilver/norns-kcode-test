8699b32924705642d01b3872ff1835198de5dbd0
Nik Silver <nik@niksilver.com>
2023-02-21 13:33:12 +0000

    Basic demo.

--- norns-kcode-test.lua
+++ norns-kcode-test.lua
@@ -2,15 +2,25 @@
 -- See https://github.com/monome/norns/issues/1657
 
 local state = {
-    kcode.key = nil,
-    kcode.value = nil,
-    kcode.code_to_char = nil,
+    kcode = {
+        key = nil,
+        value = nil,
+        code_to_char = nil
+    },
 
-    kchar.ch = nil,
-    kchar.char_to_code = nil
+    kchar = {
+        ch = nil,
+        char_to_code = nil
+    }
 }
 
+function init()
+    redraw()
+end
+
 function redraw()
+    screen.clear()
+
     screen.move(0, 8)
     screen.text("Last calls to...")
 
@@ -21,9 +31,8 @@ function redraw()
     )
 
     screen.move(0, 32)
-    screen.text("    code_to_char(" ..
-        tostring(state.kcode.key) .. ") = " ..
-        tostring(kchar.char_to_code)
+    screen.text("    code_to_char(...) = " ..
+        tostring(state.kcode.code_to_char)
     )
 
     screen.move(0, 48)
@@ -31,11 +40,12 @@ function redraw()
         tostring(state.kchar.ch) .. "')"
     )
 
-    screen.move(0, 48)
-    screen.text("    code_to_char(" ..
-        tostring(state.kchar.ch) .. ") = " ..
-        tostring(kchar.char_to_code)
+    screen.move(0, 56)
+    screen.text("    char_to_code(...) = " ..
+        tostring(state.kchar.char_to_code)
     )
+
+    screen.update()
 end
 
 function keyboard.code(key, value)
@@ -46,7 +56,7 @@ function keyboard.code(key, value)
 end
 
 function keyboard.char(ch)
-    state.kchar = ch
+    state.kchar.ch = ch
     state.kchar.char_to_code = keyboard.char_to_code(ch)
     redraw()
 end
